78659e16caf0e02e26dacbc68060ac460e89c045
Stacey Nduta <staceyndutaconsulting@gmail.com>
2025-10-02 23:11:34 +0300

    update url path

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -1,3 +1,7 @@
+from django.urls import path, include
+from api.views import BookList  # Import BookList view
+
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
+    path('api/', include('api.urls')),  # Includes URLs from the api app
 ]
