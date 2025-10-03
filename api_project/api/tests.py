eb7aeb2d8a2343c50820ffbd717328d5a79dad75
Stacey Nduta <staceyndutaconsulting@gmail.com>
2025-10-03 12:14:38 +0300

    task2

--- api_project/api/tests.py
+++ api_project/api/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class HelloWorldTest(TestCase):
+    def test_hello_world(self):
+        response = self.client.get(reverse('hello_world'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content.decode(), 'Hello, World!')
--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,19 +1,12 @@
-from django.urls import path
-from django.contrib import admin
-from django.urls import path, include  # Include function for app URLs
-from .views import BookList
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import BookViewSet
-from rest_framework.authtoken.views import obtain_auth_token
+from .views import BookList, BookViewSet
 
 
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # Include the api app's URLs under the /api/ path
     path('books/', BookList.as_view(), name='book-list'),
     path('', include(router.urls)),
-    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),  # Token retrieval URL
 ]
