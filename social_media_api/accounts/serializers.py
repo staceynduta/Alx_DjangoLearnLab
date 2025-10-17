f563539298a928262be5ea6324fe9fc3fe2fc6e0
Stacey Nduta <staceyndutaconsulting@gmail.com>
2025-10-17 19:44:54 +0300

    social media 2.0
    serializer/view updates

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,44 +1,26 @@
-from django.contrib.auth import get_user_model, authenticate
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 
-
-User = get_user_model()
-
-
-class UserSerializer(serializers.ModelSerializer):
+class CustomUserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField()
     class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'bio', 'profile_picture']
-        read_only_fields = ['id']
-
-
-class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, style={'input_type': 'password'})
-
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password', 'bio', 'profile_picture']
+        model = get_user_model()
+        fields = ['username', 'email', 'bio', 'profile_picture', 'followers', 'following']
 
     def create(self, validated_data):
-        password = validated_data.pop('password')
-        user = User(**validated_data)
-        user.set_password(password)
-        user.save()
-        Token.objects.get_or_create(user=user)
+        user = get_user_model().objects.create_user(**validated_data)
         return user
+    
+    def create(self, validated_data):
+        user = get_user_model().objects.create_user(validated_data)
+        if 'password' in validated_data:
+            user.set_password(validated_data['password'])
+            user.save()
+            token = Token.objects.create(user=user)
+        return user, token
 
-
-class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField(write_only=True, style={'input_type': 'password'})
-
-    def validate(self, attrs):
-        username = attrs.get('username')
-        password = attrs.get('password')
-        user = authenticate(username=username, password=password)
-        if not user:
-            raise serializers.ValidationError('Invalid credentials')
-        attrs['user'] = user
-        return attrs
-
+class TokenSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Token
+        fields = ('key',)
--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import RegisterView, LoginView, ProfileView, TokenView
+from .views import RegisterView, LoginView, ProfileView
+from .views import FollowUserView, UnfollowUserView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('profile/', ProfileView.as_view(), name='profile'),
-    path('token/', TokenView.as_view(), name='token'),
+   path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow-user'),
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow-user'),
 ]
-
